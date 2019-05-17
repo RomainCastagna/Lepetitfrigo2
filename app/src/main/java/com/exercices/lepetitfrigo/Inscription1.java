07cd610901f004f38d8204b7e03d67032f802484
Nelson <ribeiroteixeira.nelson@gmail.com>
2019-05-17 11:51:02 +0200

    Subscription advancement

--- app/src/main/java/com/exercices/lepetitfrigo/Inscription1.java
+++ app/src/main/java/com/exercices/lepetitfrigo/Inscription1.java
@@ -1,5 +1,6 @@
 package com.exercices.lepetitfrigo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,7 +14,8 @@ public class Inscription1 extends AppCompatActivity {
 
     public void launchSubPart2(){
         if(checkInputs()){
-
+            Intent intent = new Intent(getApplicationContext(), Inscription1.class);
+            startActivity(intent);
         }
     }
 
