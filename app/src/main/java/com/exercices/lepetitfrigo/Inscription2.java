995405c95bdfc91eb3e7371a71e30154fcc77eaf
Nelson <ribeiroteixeira.nelson@gmail.com>
2019-05-17 11:17:39 +0200

    First commit

---
+++ app/src/main/java/com/exercices/lepetitfrigo/Inscription2.java
@@ -0,0 +1,17 @@
+package com.exercices.lepetitfrigo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Inscription2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription2);
+
+
+
+
+    }
+}
